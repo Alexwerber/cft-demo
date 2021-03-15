e0828b093691969a83131ab70f8b18e95945f94b
Alexwerber <alexwerber42@gmail.com>
2021-03-15 23:06:02 +0300

    add: flags, procents, value, nominal in item.xml

--- app/src/main/java/com/example/cft_demo_final/view/adapter/ExchangeRateAdapter.java
+++ app/src/main/java/com/example/cft_demo_final/view/adapter/ExchangeRateAdapter.java
@@ -1,12 +1,15 @@
 package com.example.cft_demo_final.view.adapter;
 
 import android.annotation.SuppressLint;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -14,6 +17,7 @@ import com.example.cft_demo_final.R;
 import com.example.cft_demo_final.data.entities.ExchangeRate;
 
 import java.util.ArrayList;
+import java.util.Formatter;
 
 public class ExchangeRateAdapter extends RecyclerView.Adapter<ExchangeRateAdapter.ViewHolder> {
     private ArrayList<ExchangeRate> exchangeRates;
@@ -39,12 +43,57 @@ public class ExchangeRateAdapter extends RecyclerView.Adapter<ExchangeRateAdapte
         return new ViewHolder(view);
     }
 
+    public String clampString(String str) {
+        String showValueName;
+
+        if (str.length() > 24) {
+            showValueName = str.substring(0, 24) + "...";
+        } else showValueName = str;
+
+        return showValueName;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder (@NonNull ExchangeRateAdapter.ViewHolder holder, int position) {
-        ExchangeRate rate = exchangeRates.get(position);
+        StringBuilder sb = new StringBuilder();
+        Formatter formatter = new Formatter(sb);
 
-        holder.currencyName.setText(rate.getName());
+        ExchangeRate rate = exchangeRates.get(position);
+        // Представление вывода
+        String valueNameString = clampString(rate.getName());
+        formatter.format("%.2f ₽ / %d %s", rate.getValue(), rate.getNominal(), rate.getTicker());
+
+
+        holder.currencyName.setText(valueNameString);
+        holder.currencyRate.setText(formatter.toString());
+
+        // Вычисление процента увеличения/уменьшения стоимости валюты
+        Double percent = rate.getValue() * 100 / rate.getPreviousValue() - 100;
+        Double per =  Math.abs(Math.round(percent * 100.0) / 100.0);
+
+        holder.currencyPercent.setText(per.toString());
+
+        // Вывод стрелочки
+        if (percent < 0) {
+            holder.currencyPercentArrow.setImageResource(R.drawable.arrow_down);
+            holder.currencyPercent.setTextColor(context.getColor(R.color.decrease));
+        } else {
+            holder.currencyPercentArrow.setImageResource(R.drawable.arrow_up);
+            holder.currencyPercent.setTextColor(context.getColor(R.color.increase));
+        }
+
+        // Поиск флагов соответствующих char code самой валюты
+        int id = context.getResources().
+                getIdentifier("flag_" + rate.getTicker().toLowerCase(),
+                        "drawable",
+                        context.getPackageName());
+
+        // Вывод флага страны
+        if (id == 0)
+            holder.currencyFlag.setImageResource(R.drawable.flag_unknown);
+        else holder.currencyFlag.setImageResource(id);
     }
 
     @Override
@@ -54,10 +103,18 @@ public class ExchangeRateAdapter extends RecyclerView.Adapter<ExchangeRateAdapte
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         final TextView currencyName;
+        final TextView currencyRate;
+        final TextView currencyPercent;
+        final ImageView currencyFlag;
+        final ImageView currencyPercentArrow;
 
         public ViewHolder(View view) {
             super(view);
             currencyName = (TextView) view.findViewById(R.id.currency_name);
+            currencyRate = (TextView) view.findViewById(R.id.currency_rate);
+            currencyPercent = (TextView) view.findViewById(R.id.currency_percent);
+            currencyFlag = (ImageView) view.findViewById(R.id.currency_flag);
+            currencyPercentArrow = (ImageView) view.findViewById(R.id.currency_percent_arrow);
         }
     }
 }
