ab9ec082504c8c47231ee6093f34c41d27bb42cb
Alexwerber <alexwerber42@gmail.com>
2021-03-15 16:33:14 +0300

    add: retrofit, okhttp, gson libraries; make basic structure

--- app/src/main/java/com/example/cft_demo_final/view/activity/MainActivity.java
+++ app/src/main/java/com/example/cft_demo_final/view/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.cft_demo_final;
+package com.example.cft_demo_final.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.cft_demo_final.R;
 
 public class MainActivity extends AppCompatActivity {
 
